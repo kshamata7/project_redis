f46f773a5a3ae695bd15efd77152f46bb6da2346
kshamata7 <139546066+kshamata7@users.noreply.github.com>
2024-10-05 20:36:48 +0530

    Update variables.tf

--- terraform/ec2/variables.tf
+++ terraform/ec2/variables.tf
@@ -7,6 +7,11 @@ variable "public_subnet_id" {
 variable "private_subnet_id" {
     type = string
 }
+
+variable "private_subnet_02_id" {
+    type = string
+  
+}
 variable "bastion_sg_id" {
     type = string
 }
