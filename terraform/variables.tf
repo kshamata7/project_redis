4078cab35d3f1aa378bafc6cc91a6e04e84eeb39
kshamata7 <139546066+kshamata7@users.noreply.github.com>
2024-10-05 21:13:00 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,8 +10,3 @@ variable "peer_vpc_id" {
   description = "The ID of the existing VPC to peer with"
   default     = "vpc-09516b6e0d869c1d8"
 }
-
-variable "private_subnet_02_id" {
-  type = string
-  
-}
